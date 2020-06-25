073a6f0fcee212c4e9b2b65756980a711da310d2
theInevitable007 <beingadithya06@gmail.com>
2020-06-25 12:16:25 +0530

    F

--- SupplyMission-master/sketch.js
+++ SupplyMission-master/sketch.js
@@ -27,23 +27,18 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-
 	engine = Engine.create();
 	world = engine.world;
 
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:true});
 	World.add(world, packageBody);
 	
-
-	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
 
 	Engine.run(engine);
   
 }
-
 
 function draw() {
   rectMode(CENTER);
